f682b2e2d94df9fb27c67f068bd17b415ac13b4b
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2022-10-25 20:33:45 -0300

    code
    code

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,29 @@
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
     cliente;
-    saldo = 0;
+    saldo = 0 ;
+
+
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this.cliente = novoValor;
+        }
+    }
+    get cliente() {
+        return this.cliente;
+    }
+
+    constructor(agencia, cliente) {
+
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+
+    }
+
+
 
     sacar(valor) {
         if (this.saldo >= valor) {
@@ -11,20 +33,17 @@ export class ContaCorrente {
     }
     depositar(valor) {
         if (valor <= 0) {
-            return;   
+            return;
         }
         else {
-            this.saldo += valor; 
+            this.saldo += valor;
         }
     }
     transferir(valor, conta) {
 
         conta.cidade = "Sao Paulo";
         const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado); 
-       
-      
+        conta.depositar(valorSacado);
 
-        
     }
 }
--- index.js
+++ index.js
@@ -2,36 +2,21 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 // criando novos clientes
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11223344;
-
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 4433221122;
-
-
-//const contaCorrenteRicardo = new ContaCorrente();
-//contaCorrenteRicardo.agencia = 1001;
-//contaCorrenteRicardo.cliente = cliente1;
-//contaCorrenteRicardo.depositar(500);
-
-const conta1 = new ContaCorrente();
-conta1.cliente = cliente1;
-conta1.agencia = 103;
+const cliente1 = new Cliente("Ricardo", 112233445566);
+const cliente2 = new Cliente("Alice", 4433221122);
+const cliente3 = new Cliente("André", 1122334477);
 
 
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+// criando novas CONTAS 
+const conta1 = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(1002, cliente2);
+const conta3 = new ContaCorrente(1003, cliente3);
+
 
 conta1.depositar(500);
-conta2.depositar(600);
-conta1.transferir(200, conta2);
 
-//let valor = 200;
-//contaCorrenteRicardo.transferir(valor, conta2);
+
 
 console.log(conta1);
-console.log(conta2);
+console.log(ContaCorrente.numeroDeContas );
