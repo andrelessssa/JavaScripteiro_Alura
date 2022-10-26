2e0ab76baec2cce5419d49983abf938a1be2903c
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2022-10-26 18:24:08 -0300

    code
    code

--- OrdemDeServico/index.js
+++ OrdemDeServico/index.js
@@ -1,14 +1,15 @@
 import { Cliente } from "./Cliente";
-//import { OrdemServico } from "./OrdemServico";
+import { OrdemServico } from "./OrdemServico";
 
 
 //nome, cpf, endereco, telefone//
-const cliente1 = new Cliente("André",11223333444,"Rua B,Sao Jorge",82981121510);
+const cliente1 = new Cliente("André", 11223333444, "Rua B,Sao Jorge", 82981121510);
+const cliente2 = new Cliente("AndrÉ",11223333444,"Rua B,Sao Jorge",82981121510);
 
 
 
 //aparelho, marca, modelo, senha, problemaRelatado, _imei, valor//
-//const ordemServico1 = new OrdemServico("SmartPhone","iPhone","iphone 8",12345,"tela Quebrada", 11223344,"450,00");
+const ordemServico1 = new OrdemServico("SmartPhone","iPhone","iphone 8",12345,"tela Quebrada", 11223344,"450,00");
 
 
 
